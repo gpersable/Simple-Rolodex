50b24709f0ab9b0e6d4151f2622417f95a7b96bd
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2020-12-14 11:20:45 +0700

    Add fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      people: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(users => this.setState({people: users}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.people.map(people => (
+            <h1 key={people.id}> {people.name} </h1>
+            )
+          )
+        }
+      </div>
+    );
+  }
 }
+
 
 export default App;
