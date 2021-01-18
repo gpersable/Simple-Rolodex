fdff2c30e2cb1caf47bcd312879d0d802e8ba18e
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2021-01-18 14:09:36 +0700

    Finishing touch

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
 
     return (
       <div className="App">
+      <h1>People Rolodex</h1> 
       <SearchBox 
         placeholder="Search someone..."
         handleChange={this.handleChange} 
